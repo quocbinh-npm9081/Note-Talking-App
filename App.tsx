c62d17e2f7b5c9a9a4c583b6b2cfbbebd79ebb54
louis <quocbinhgithub@gmail.com>
2023-06-04 21:43:32 +0700

    create repo app

--- App.tsx
+++ App.tsx
@@ -1,11 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, TextInput } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
+      <TextInput
+        textAlignVertical="top"
+        multiline={true}
+        style={styles.textInput}
+      ></TextInput>
     </View>
   );
 }
@@ -13,8 +18,15 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  textInput: {
+    backgroundColor: "#ffb70324",
+    fontSize: 16,
+    width: "100%",
+    height: 200,
+    paddingHorizontal: 20,
   },
 });
