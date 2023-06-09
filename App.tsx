c36c9232f4a22dcb18f1e534a6b2c6858f72c886
louis <quocbinhgithub@gmail.com>
2023-06-09 23:02:14 +0700

    feat(EditNoteScreen.tsx): add useLayoutEffect

--- App.tsx
+++ App.tsx
@@ -1,10 +1,10 @@
-import React, { useState } from "react";
-import { Button, StyleSheet } from "react-native";
+import React from "react";
+import { StyleSheet } from "react-native";
 import HomeScreen from "./Screens/HomeScreen";
-import { NavigationContainer } from "@react-navigation/native";
+import { NavigationContainer, useNavigation } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import EditNoteScreen from "./Screens/EditNoteScreen";
-import { RootStackParamList } from "./types";
+import { RootStackParamList, ScreenNavigastionProps } from "./types";
 import NewNoteButton from "./Components/NewNoteButton";
 export default function App() {
   const Stack = createNativeStackNavigator<RootStackParamList>();
--- Screens/EditNoteScreen.tsx
+++ Screens/EditNoteScreen.tsx
@@ -1,12 +1,18 @@
 import React from "react";
 import { NoteTalkingInput } from "../Components/NoteTalkingInput";
-import { saveNote } from "../Services/noteStoreService";
-import { useRoute } from "@react-navigation/native";
-import { EditScreenRouteProps } from "../types";
+import { useRoute, useNavigation } from "@react-navigation/native";
+import { EditScreenRouteProps, ScreenNavigastionProps } from "../types";
 const EditNoteScreen: React.FC = () => {
   const route = useRoute<EditScreenRouteProps>();
+  const navigation = useNavigation<ScreenNavigastionProps>();
   const noteId = route.params.noteId;
-  return <NoteTalkingInput saveNote={saveNote} noteId={noteId} />;
+
+  React.useLayoutEffect(() => {
+    navigation.setOptions({
+      headerTitle: noteId ? "Edit note" : "New note",
+    });
+  }, [navigation]);
+  return <NoteTalkingInput noteId={noteId} />;
 };
 
 export default EditNoteScreen;
